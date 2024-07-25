a48f367c17397b191dc3f20c8de3685e6dba6ea2
LoseYourSelf12 <loseyourself1290@gmail.com>
2024-07-25 13:42:30 +0300

    ~

--- WS_test.py
+++ WS_test.py
@@ -1,14 +1,24 @@
 import websocket
 import json
 
-WS = 'ws://194.87.111.128:1031/'
+def on_message(ws, message):
+    data = json.loads(message)
+    print(f"Received message: {data}")
 
-con = websocket.create_connection(WS)
+def on_error(ws, error):
+    print(f"Error: {error}")
 
-message = con.recv()
+def on_close(ws, close_status_code, close_msg):
+    print(f"Connection closed: {close_status_code}, {close_msg}")
 
-data = json.loads(message)
+def on_open(ws):
+    print("Connection opened")
 
-print(data)
-
-con.close()
+if __name__ == "__main__":
+    websocket.enableTrace(True)
+    ws = websocket.WebSocketApp("ws://194.87.111.128:1031",
+                                on_open=on_open,
+                                on_message=on_message,
+                                on_error=on_error,
+                                on_close=on_close)
+    ws.run_forever()
