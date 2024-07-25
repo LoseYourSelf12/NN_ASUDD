02dccf44b756b9cb724ad2ed8512a1e8899e2f2b
LoseYourSelf12 <loseyourself1290@gmail.com>
2024-07-25 10:18:45 +0300

    WS check

---
+++ WS.py
@@ -0,0 +1,14 @@
+import websocket
+import json
+
+WS = 'ws://194.87.111.128:1031'
+
+con = websocket.create_connection(WS)
+
+message = con.recv()
+
+data = json.loads(message)
+
+print(data)
+
+con.close()
